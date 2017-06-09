2b476240e1826236b725024533f716b228b6b770
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-06-09 16:47:32 +0700

    feat: Get X-Line-Signature

--- helloworld.go
+++ helloworld.go
@@ -2,10 +2,14 @@ package main
 
 import (
     "io"
+    "io/ioutil"
     "net/http"
     "fmt"
-    //"os"
-    //"log"
+    "os"
+    "log"
+    "encoding/base64"
+    "crypto/hmac"
+    "crypto/sha256"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -15,16 +19,18 @@ func hello(w http.ResponseWriter, r *http.Request) {
     }
 }
 
-func callback() {
+func callback(w http.ResponseWriter, req *http.Request) {
     defer req.Body.Close()
     body, err := ioutil.ReadAll(req.Body)
     if err != nil {
       fmt.Println(err)
     }
+    fmt.Println(body)
     decoded, err := base64.StdEncoding.DecodeString(req.Header.Get("X-Line-Signature"))
     if err != nil {
       fmt.Println(err)
     }
+    fmt.Println(decoded)
     hash := hmac.New(sha256.New, []byte("6db72166ed2b37fbfd0a4a00f7bd01ac"))
     hash.Write(body)
 }
