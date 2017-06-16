67a517372d406c07f4137efb2899523f492f7e53
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-06-16 10:52:21 +0700

    feat: give choice of numbers of players

--- helloworld.go
+++ helloworld.go
@@ -10,7 +10,7 @@ import (
     "github.com/line/line-bot-sdk-go/linebot"
 )
 
-var userChoiceMap map[string]string
+var userChoiceMap map[string]map[string]string
 
 func hello(w http.ResponseWriter, r *http.Request) {
     name := r.URL.Query().Get("name")
@@ -52,41 +52,55 @@ func callback(w http.ResponseWriter, req *http.Request) {
                           log.Print(err)
                       }
       }
+    // case linebot.EventTypePostback:
+    //   if event.Source.Type == linebot.EventSourceTypeGroup {
+    //     runHompimpaGame(event.Source.GroupID, event.Source.UserId, event.Postback.Data)
+    //   } else if event.Source.Type == linebot.EventSourceTypeRoom {
+    //     runHompimpaGame(event.Source.RoomID, event.Source.UserId, event.Postback.Data)
+    //   } else if event.Source.Type == linebot.EventSourceTypeUser {
+    //     if _, err = bot.ReplyMessage(
+    //                           event.ReplyToken,
+    //                           linebot.NewTextMessage("Kamu cuma bisa main hompimpa di group atau room"),
+    //                           ).Do(); err != nil {
+    //                               log.Print(err)
+    //                           }
+    //   }
     case linebot.EventTypeMessage:
       switch message := event.Message.(type) {
       case *linebot.TextMessage:
         if (strings.Contains(message.Text, "@bot") && strings.Contains(message.Text, "hompimpa")) {
-          // if strings.Contains(message.Text, "hompimpa") {
-            template := linebot.NewConfirmTemplate(
-			                          "Mau pilih apa?",
-			                          linebot.NewPostbackTemplateAction("Putih", "Putih", ""),
-			                          linebot.NewPostbackTemplateAction("Hitam", "Hitam", ""),
+          template := linebot.NewButtonsTemplate(
+			                          "", "", "Mau pilih apa?",
+			                          linebot.NewPostbackTemplateAction("3", "numberOfPlayers=3", "3"),
+			                          linebot.NewPostbackTemplateAction("4", "numberOfPlayers=4", "4"),
+                                linebot.NewPostbackTemplateAction("5", "numberOfPlayers=5", "5"),
+                                linebot.NewPostbackTemplateAction("6", "numberOfPlayers=6", "6"),
 		                            )
-		        if _, err := bot.ReplyMessage(
+		      if _, err := bot.ReplyMessage(
 			                          event.ReplyToken,
 			                          linebot.NewTemplateMessage("Hompimpa", template),
 		                            ).Do(); err != nil {
 			                             log.Print(err)
 		                            }
-          } else {
-            if _, err = bot.ReplyMessage(
+        } else {
+          if _, err = bot.ReplyMessage(
                                 event.ReplyToken,
                                 linebot.NewTextMessage("Wah, ak gak ngerti kamu mau apa, aku cuma bisa kasih game hompimpa"),
                                 ).Do(); err != nil {
                                     log.Print(err)
                                 }
-          // }
         }
       }
     }
   }
 }
 
-func runHompimpaGame() {
+func runHompimpaGame(group_id, user_id, choice string) {
 
 }
 
 func main() {
+    userChoiceMap = make(map[string]map[string]string)
 
     port := os.Getenv("PORT")
     if port == "" {
