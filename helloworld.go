6259753b0f37fdf5236890c76ebb2821d69b9b17
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-06-16 12:41:12 +0700

    debugging: event json

--- helloworld.go
+++ helloworld.go
@@ -35,6 +35,7 @@ func callback(w http.ResponseWriter, req *http.Request) {
     return
   }
   for _, event := range events {
+    fmt.Println(event)
     fmt.Println(event.Type)
     switch event_type := event.Type; event_type {
     case linebot.EventTypeJoin:
